b2831a70da6dd815bc6e2d7be3f7737e81e167e1
vika3108 <vika.antonova1997@gmail.com>
2022-10-09 21:04:13 +0300

    Решили задачу 13

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@ string stringNumber = Convert.ToString(threeDigitNumber);
 Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
 */
 
+/* Задача № 13
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
+*/
