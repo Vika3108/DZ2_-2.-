42632fac7399846d4e02c4b45d40d5fd4f0622f1
vika3108 <vika.antonova1997@gmail.com>
2022-10-09 21:01:47 +0300

    Решили десятую задачу

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/* Задача № 10
+Console.WriteLine ("Введите трехзначное число");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+*/
+
